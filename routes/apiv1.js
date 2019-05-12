1419dff837b2096e2dbe27c52a51947f93adbd0c
Christian Richardson <chrisrichar@hotmail.com>
2019-05-12 11:37:20 +0000

    COMPX341-19 A3 VERSION 1 Added metric unit display and functionality for NZ cities

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -7,7 +7,7 @@ var request = REQUEST.defaults( {
     strictSSL: false
 });
 
-var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
+var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric";
 
 exports.getWeather = function(req, res) {
 	var zip = req.query.zip;
@@ -16,6 +16,9 @@ exports.getWeather = function(req, res) {
 	}
 
 	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	
+	if(zip.length == 4)
+		aurl = OPENWEATHERURL + '&zip=' + zip + ',nz';
 
 	request({
 		method: 'GET',
@@ -47,6 +50,9 @@ exports.getWeather2 = function(req, res) {
 	}
 
 	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	
+	if(zip.length == 4)
+		aurl = OPENWEATHERURL + '&zip=' + zip + ',nz';
 
 	request({
 		method: 'GET',
