e9223eb7fe7be0d576631ceed7f0bd8da4f18d15
Christian Richardson <chrisrichar@hotmail.com>
2019-05-14 00:03:20 +0000

    adding new test for a city that doesnt exist

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -72,25 +72,23 @@
         });
     	});
 
-      /*it('with another valid zip code', function(done) {
+		it('with nonexistant city', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
         }
         request({
       		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?zip=1010'
-          }, function(err, resp, body) {
+              url: appUrl + '/api/v1/getWeather?zip=blarg'
+          }, /* @callback */ function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
           	} else {
-              assert.equal(resp.statusCode, 200);
-              var pbody = JSON.parse(body);
-              assert(pbody.city === 'Auckland', "City name does not match");
+              assert.equal(resp.statusCode, 400);
               done();
             }
         });
-    	});*/
+    	});
     	
     });
 })();
