623a734912c3a18039b9b060f321590e7fd701e5
Christian Richardson <chrisrichar@hotmail.com>
2019-05-15 05:38:32 +0000

    change the way we read coord parameter

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -35,8 +35,9 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
                 method: "GET",
                 url: '/api/v1/getWeather?zip=' + data
             }).then( function(response) {
+            	var coords = response.data.coord;
                 if(which === 1) {
-                    $scope.zip1City = response.data.coord['lat'] + "," + response.data.coord['lon']; //response.data.coord['lat'] + "," + response.data.coord['lon'];
+                    $scope.zip1City = coords['lat'] + "," + coords['lon']; //response.data.coord['lat'] + "," + response.data.coord['lon'];
                     $scope.zip1Weather = response.data.weather;
                 } else if(which === 2) {
                     $scope.zip2City = response.data.city;
